ea0ae8c8819e628febb516e38cf2ecc958c186ac
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-18 16:22:26 +0100

    Draws one tile with separate classes

--- map.py
+++ map.py
@@ -6,11 +6,14 @@ class Map():
     def __init__(self):
         pass
 
-    def draw_main(self):
-        self.root = Tk()
-        self.canvas = Canvas(self.root, width = 720, height = 720)
-        self.canvas.pack()
-        self.root.mainloop()
+    def draw_tile(self, canvas, floor1):
+        self.tile = canvas.create_image(floor1.tile_x, floor1.tile_y, image = floor1.image)
 
+root = Tk()
+canvas = Canvas(root, width = 720, height = 720)
+canvas.pack()
+
+floor1 = Floor()
 map = Map()
-map.draw_main()
+map.draw_tile(canvas, floor1)
+root.mainloop()
--- tile.py
+++ tile.py
@@ -2,21 +2,20 @@ from tkinter import *
 
 class Tile():
 
-    def __init__(self):
-        self.image = image
-        self.size = 72
+    def __init__(self, size = 72, type = 'penetrable', tile_x = 100, tile_y = 100):
+        self.size = size
         self.type = type
         self.tile_x = tile_x
         self.tile_y = tile_y
 
 class Floor(Tile):
 
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, size = 72, type = 'penetrable', tile_x = 100, tile_y = 100):
+        super().__init__(size, type, tile_x, tile_y)
         self.image = PhotoImage(file = 'floor_tile.png')
 
 class Wall(Tile):
 
-    def __init__(self):
-        super().__init__(self)
+    def __init__(self, size = 72, type = 'penetrable', tile_x = 100, tile_y = 100):
+        super().__init__(size, type, tile_x, tile_y)
         self.image = PhotoImage(file = 'wall_tile.png')
