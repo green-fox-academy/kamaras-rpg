5abc337bbb30172d87aac54b7bb90ac557749b73
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-18 17:10:33 +0100

    Fully draws the grid with loops

--- map.py
+++ map.py
@@ -12,23 +12,24 @@ class Map():
         global wall1
         wall1 = Wall()
         grid = [
-            [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
-            [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0, 1, 1, 0, 1, 0],
+            [1, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+            [1, 0, 1, 1, 1, 0, 1, 0, 1, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
             [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
             [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
             [1, 0, 1, 0, 0, 0, 1, 0, 0, 0],
         ]
-        for line in grid:
+        for j in range(10):
+            line = grid[j]
             for i in range(10):
                 if line[i] == 0:
-                    self.floor = canvas.create_image(floor1.tile_x + i * 72, floor1.tile_y, image = floor1.image)
+                    self.floor = canvas.create_image(floor1.tile_x + i * 72, floor1.tile_y + j * 72, image = floor1.image)
                 else:
-                    self.wall = canvas.create_image(wall1.tile_x + i * 72, wall1.tile_y + 72, image = wall1.image)
+                    self.wall = canvas.create_image(wall1.tile_x + i * 72, wall1.tile_y + j * 72, image = wall1.image)
 
 root = Tk()
 canvas = Canvas(root, width = 720, height = 720)
