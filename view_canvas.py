4f0deb57c5436536f4758e38f564e7d2278547b9
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-18 14:40:24 +0100

    Began to add class variables

--- view_canvas.py
+++ view_canvas.py
@@ -1,21 +1,36 @@
 from tkinter import *
 
-class Map():
+class Tile():
 
     def __init__(self, canvas):
         self.canvas = canvas
+        self.size = 72
+        self.base_xy = 38
+        self.tile_type = ''
+        self.image = ''
     
-    def draw_main(self):
-        self.img = PhotoImage(file = 'floor_tile.png')
-        for i in range(0, 720, 72):
-            for j in range(0, 720, 72):
-                self.image = self.canvas.create_image(38 + i, 38 + j, image = self.img)
+# class Floor(Tile):
+    
+#     def __init__(self, canvas):
+#         super().__init__(self)
+#         self.image = PhotoImage(file = 'floor_tile.png')
+    
+    def draw_floor(self):
+        self.img_floor = PhotoImage(file = 'floor_tile.png')
+        for i in range(0, self.size * 10, self.size):
+            for j in range(0, self.size * 10, self.size):
+                self.floor = self.canvas.create_image(self.base_xy + i, self.base_xy + j, image = self.img_floor)
+    
+    def draw_wall(self):
+        self.img_wall = PhotoImage(file = 'wall_tile.png')
+        self.wall = self.canvas.create_image(110, 110, image = self.img_wall)
 
 root = Tk()
 my_canvas = Canvas(root, width = 720, height = 720)
 my_canvas.pack()
 
-object = Map(my_canvas)
-object.draw_main()
+object = Tile(my_canvas)
+object.draw_floor()
+object.draw_wall()
 
 root.mainloop()
