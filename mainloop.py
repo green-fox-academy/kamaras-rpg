44a0bb916b8663ab2dc9635d965f28ac3bd59bfe
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-19 16:22:02 +0100

    Hero moving methods entirely operated from mainloop

--- mainloop.py
+++ mainloop.py
@@ -10,10 +10,25 @@ map.draw_tile(canvas)
 
 my_hero = Hero(canvas, map)
 
-canvas.bind('<KeyPress>', my_hero.move_by_keys)
+def draw_hero():
+    map.draw_tile(canvas)
+    canvas.create_image(my_hero.char_x, my_hero.char_y, image = my_hero.image)
+
+def move_by_keys(e):
+    if e.keycode == 38:
+        my_hero.move_up()
+    elif e.keycode == 40:
+        my_hero.move_down()
+    elif e.keycode == 39:
+        my_hero.move_right()
+    elif e.keycode == 37:
+        my_hero.move_left()
+    draw_hero()
+
+canvas.bind('<KeyPress>', move_by_keys)
 canvas.pack()
 canvas.focus_set()
 
-my_hero.draw_hero()
+draw_hero()
 
 root.mainloop()
--- model_hero.py
+++ model_hero.py
@@ -3,26 +3,22 @@ from model_character import *
 
 class Hero(Character):
 
-    def __init__(self, canvas, map, random_num = 0, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
+    def __init__(self, map, random_num = 0, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
         super().__init__(random_num, char_x, char_y, hp, dp, sp)
         self.hp = self.hp * 3 + 20
         self.dp = self.dp * 2
         self.sp = self.sp + 5
         self.image = PhotoImage(file = 'hero-down.png')
-        self.canvas = canvas
         self.map = map
+    
+    def move_up(self):
+        self.char_y = self.char_y - 72
+    
+    def move_down(self):
+        self.char_y = self.char_y + 72
 
-    def draw_hero(self):
-        self.map.draw_tile(self.canvas)
-        self.canvas.create_image(self.char_x, self.char_y, image = self.image)
-
-    def move_by_keys(self, e):
-        if e.keycode == 38:
-            self.char_y = self.char_y - 72
-        elif e.keycode == 40:
-            self.char_y = self.char_y + 72
-        elif e.keycode == 39:
-            self.char_x = self.char_x + 72
-        elif e.keycode == 37:
-            self.char_x = self.char_x - 72
-        self.draw_hero()
+    def move_right(self):
+        self.char_x = self.char_x + 72
+    
+    def move_left(self):
+        self.char_x = self.char_x - 72
