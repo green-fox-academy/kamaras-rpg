8de7f371e8cf25aa81869ba73d13c6420da48122
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-19 15:04:07 +0100

    Multiply the hero by keypresses

--- mainloop.py
+++ mainloop.py
@@ -1,14 +1,19 @@
+from tkinter import *
 from map import *
 from model_hero import *
 
 root = Tk()
 canvas = Canvas(root, width = 720, height = 720)
-canvas.pack()
 
 map = Map()
 map.draw_tile(canvas)
 
-my_hero = Hero()
-my_hero.draw_hero(canvas)
+my_hero = Hero(canvas)
+
+canvas.bind('<KeyPress>', my_hero.move_by_keys)
+canvas.pack()
+canvas.focus_set()
+
+my_hero.draw_hero()
 
 root.mainloop()
--- model_hero.py
+++ model_hero.py
@@ -3,17 +3,18 @@ from model_character import *
 
 class Hero(Character):
 
-    def __init__(self, random_num = 0, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
+    def __init__(self, canvas, random_num = 0, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
         super().__init__(random_num, char_x, char_y, hp, dp, sp)
         self.hp = self.hp * 3 + 20
         self.dp = self.dp * 2
         self.sp = self.sp + 5
         self.image = PhotoImage(file = 'hero-down.png')
+        self.canvas = canvas
 
-    def draw_hero(self, canvas):
-        canvas.create_image(self.char_x, self.char_y, image = self.image)
+    def draw_hero(self):
+        self.canvas.create_image(self.char_x, self.char_y, image = self.image)
 
-    def move_by_keys(e):
+    def move_by_keys(self, e):
         if e.keycode == 38:
             self.char_y = self.char_y - 72
         elif e.keycode == 40:
@@ -22,3 +23,4 @@ class Hero(Character):
             self.char_x = self.char_y + 72
         elif e.keycode == 37:
             self.char_x = self.char_x - 72
+        self.draw_hero()
