d36eefd06c50ed312b91045af665377dc6066f77
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-19 11:13:26 +0100

    Character modul with random default params

---
+++ model_character.py
@@ -0,0 +1,20 @@
+import random
+
+class Character():
+
+    def __init__(self, random_num = 0, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
+        self.random_num = random.randint(1, 6)
+        self.char_x = char_x
+        self.char_y = char_y
+        self.hp = self.random_num
+        self.dp = self.random_num
+        self.sp = self.random_num
+    
+    # def random_multiplier(self):
+    #     self.rand = random.randint(1, 6)
+    #     return self.rand
+
+object = Character()
+print(object.hp)
+print(object.dp)
+print(object.sp)
