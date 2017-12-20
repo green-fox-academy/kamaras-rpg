90281e6ab26a114d323feaacac178a93f8daf9e9
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-20 12:35:53 +0100

    Feat: The Hero moves only on floor, stops at wall

--- mainloop.py
+++ mainloop.py
@@ -7,6 +7,7 @@ canvas = Canvas(root, width = 720, height = 720)
 
 map = Map()
 map.draw_tile(canvas)
+temp_walls = map.draw_tile(canvas)
 
 my_hero = Hero(canvas)
 
@@ -15,13 +16,13 @@ def draw_hero():
     canvas.create_image(my_hero.char_x, my_hero.char_y, image = my_hero.image)
 
 def move_by_keys(e):
-    if e.keycode == 38 and my_hero.char_y > 38:
+    if e.keycode == 38 and my_hero.char_y > 38 and [my_hero.char_x, my_hero.char_y - 72] not in temp_walls:
         my_hero.move_up()
-    elif e.keycode == 40 and my_hero.char_y < 682 :
+    elif e.keycode == 40 and my_hero.char_y < 682 and [my_hero.char_x, my_hero.char_y + 72] not in temp_walls:
         my_hero.move_down()
-    elif e.keycode == 39 and my_hero.char_x < 682:
+    elif e.keycode == 39 and my_hero.char_x < 682 and [my_hero.char_x + 72, my_hero.char_y] not in temp_walls:
         my_hero.move_right()
-    elif e.keycode == 37 and my_hero.char_x > 38:
+    elif e.keycode == 37 and my_hero.char_x > 38 and [my_hero.char_x - 72, my_hero.char_y] not in temp_walls:
         my_hero.move_left()
     draw_hero()
 
--- map.py
+++ map.py
@@ -18,9 +18,15 @@ class Map():
         global wall1
         wall1 = Wall()
         grid = self.read()
+        walls = []
         for j in range(len(grid)):
             for i in range(len(grid[j])):
                 if grid[j][i] == '0':
                     canvas.create_image(floor1.tile_x + i * 72, floor1.tile_y + j * 72, image = floor1.image)
                 elif grid[j][i] == '1':
                     canvas.create_image(wall1.tile_x + i * 72, wall1.tile_y + j * 72, image = wall1.image)
+                    one_wall = []
+                    one_wall.append(wall1.tile_x + i * 72)
+                    one_wall.append(wall1.tile_y + j * 72)
+                    walls.append(one_wall)
+        return walls
