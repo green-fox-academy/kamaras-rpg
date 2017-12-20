1d5c77d7a8faaec692bfd7cad10e84e686b18533
tamaskamaras <kamaras.tamas.viktor@gmail.com>
2017-12-20 15:59:02 +0100

    feat: monster addition

--- mainloop.py
+++ mainloop.py
@@ -1,35 +1,42 @@
 from tkinter import *
 from map import *
 from model_hero import *
+from model_monster import *
 
 root = Tk()
 canvas = Canvas(root, width = 720, height = 720)
 
 map = Map()
 map.draw_tile(canvas)
-temp_walls = map.draw_tile(canvas)
+walls = map.draw_tile(canvas)
 
 my_hero = Hero(canvas)
+skeleton1 = Monster()
+skeleton2 = Monster()
+boss = Boss()
 
-def draw_hero():
+def draw_characters():
     map.draw_tile(canvas)
+    canvas.create_image(skeleton1.char_x + 72, skeleton1.char_y + 576, image = skeleton1.image)
+    canvas.create_image(skeleton1.char_x + 216, skeleton1.char_y, image = skeleton1.image)
+    canvas.create_image(boss.char_x + 576, boss.char_y + 504, image = boss.image)
     canvas.create_image(my_hero.char_x, my_hero.char_y, image = my_hero.image)
 
 def move_by_keys(e):
-    if e.keycode == 38 and my_hero.char_y > 38 and [my_hero.char_x, my_hero.char_y - 72] not in temp_walls:
+    if e.keycode == 38 and my_hero.char_y > 38 and [my_hero.char_x, my_hero.char_y - 72] not in walls:
         my_hero.move_up()
-    elif e.keycode == 40 and my_hero.char_y < 682 and [my_hero.char_x, my_hero.char_y + 72] not in temp_walls:
+    elif e.keycode == 40 and my_hero.char_y < 682 and [my_hero.char_x, my_hero.char_y + 72] not in walls:
         my_hero.move_down()
-    elif e.keycode == 39 and my_hero.char_x < 682 and [my_hero.char_x + 72, my_hero.char_y] not in temp_walls:
+    elif e.keycode == 39 and my_hero.char_x < 682 and [my_hero.char_x + 72, my_hero.char_y] not in walls:
         my_hero.move_right()
-    elif e.keycode == 37 and my_hero.char_x > 38 and [my_hero.char_x - 72, my_hero.char_y] not in temp_walls:
+    elif e.keycode == 37 and my_hero.char_x > 38 and [my_hero.char_x - 72, my_hero.char_y] not in walls:
         my_hero.move_left()
-    draw_hero()
+    draw_characters()
 
 canvas.bind('<KeyPress>', move_by_keys)
 canvas.pack()
 canvas.focus_set()
 
-draw_hero()
+draw_characters()
 
 root.mainloop()
--- model_monster.py
+++ model_monster.py
@@ -3,8 +3,8 @@ from model_character import *
 
 class Monster(Character):
 
-    def __init__(self, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
-        super().__init__(char_x, char_y, hp, dp, sp)
+    def __init__(self, random_num = 0, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
+        super().__init__(random_num, char_x, char_y, hp, dp, sp)
         self.lvl_number = 1
         self.hp = 2 * self.lvl_number * self.hp
         self.dp = int((self.lvl_number / 2) * self.dp)
@@ -13,8 +13,8 @@ class Monster(Character):
 
 class Boss(Monster):
 
-    def __init__(self, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
-        super().__init__(char_x, char_y, hp, dp, sp)
+    def __init__(self, random_num = 0, char_x = 38, char_y = 38, hp = 0, dp = 0, sp = 0):
+        super().__init__(random_num, char_x, char_y, hp, dp, sp)
         self.lvl_number = 1
         self.hp = self.hp + self.lvl_number
         self.dp = self.dp + self.lvl_number
